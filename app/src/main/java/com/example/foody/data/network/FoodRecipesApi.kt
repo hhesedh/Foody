b213d3ac85bfcf1ceec6ce4577536295679a08bc
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-02-13 20:43:59 -0300

    criação  de métodos para buscar dados da api e inversão de dependencias

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -1,4 +1,4 @@
-package com.example.foody
+package com.example.foody.data.network
 
 import com.example.foody.models.FoodRecipe
 import retrofit2.Response
@@ -7,7 +7,7 @@ import retrofit2.http.QueryMap
 
 interface FoodRecipesApi {
     @GET("/recipes/complexSearch")
-    fun getRecipes(
+    suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
 }
