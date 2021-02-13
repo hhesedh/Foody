8922ddf11d99c702656319561c91d8ee1e88eca4
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-02-13 17:07:05 -0300

    refatoração e criação de classe de constants

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "533b0b66615849f6b03a775b698069a0"
+    }
+}
