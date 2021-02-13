2408bdfd61712961a6e50f511116ca05ac23aa6e
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-02-13 15:32:13 -0300

    design do RecipesRowLayout

--- app/src/main/java/com/example/foody/MainActivity.kt
+++ app/src/main/java/com/example/foody/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-         navController = findNavController(R.id.navHostFragment)
+        navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.recipesFragment,
