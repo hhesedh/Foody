b8fd21df70f6974a33d3ef48d0d652f8a10bf961
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-02-13 16:20:56 -0300

    design do recyclerView placeholder shimmer

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,17 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foody.databinding.FragmentRecipesBinding
 
 
 class RecipesFragment : Fragment() {
 
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() =  _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    ): View {
+        _binding =  FragmentRecipesBinding.inflate(inflater, container, false)
+
+        binding.recyclerview.showShimmer()
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
